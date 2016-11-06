45dade5ab50b5f65d057365722975b7947c24639
Tom Hazledine <tom@tomhazledine.com>
2016-11-06 22:28:53 +0000

    non-wp jquery

--- inc/enqueuing.php
+++ inc/enqueuing.php
@@ -11,7 +11,13 @@
 function tomhazledine_theme_scripts() {
 	wp_enqueue_style( 'tomhazledine_theme-style', get_template_directory_uri() . '/assets/css/main.min.css' );
 
-	wp_enqueue_script( 'tomhazledine_theme-scripts', get_template_directory_uri() . '/assets/js/app.js', array( 'jquery' ), '20151215', true );
+    // Use a non-WP version of jquery (to get around the native .noConflict() version that WP uses by default).
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', ("//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"), false, '1.11.2', true);
+    wp_enqueue_script('jquery');
+
+	// wp_enqueue_script( 'tomhazledine_theme-scripts', get_template_directory_uri() . '/assets/js/app.js', array( 'jquery' ), '20151215', true );
+    wp_enqueue_script( 'tomhazledine_theme-scripts', get_template_directory_uri() . '/assets/js/app.js', array( 'jquery' ) );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
