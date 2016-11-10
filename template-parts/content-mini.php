730c3151ccf155d667d78b9bffe1328d4464a055
Tom Hazledine <tom@tomhazledine.com>
2016-11-10 09:44:11 +0000

    rejiggering archive posts markup

--- template-parts/content-mini.php
+++ template-parts/content-mini.php
@@ -8,33 +8,15 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <?php
-        the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+    <h3 class="entry-title"><a href="<?= esc_url( get_permalink() ); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 
-        if ( 'post' === get_post_type() ) : ?>
-        <div class="entry-meta">
-            <?php //tomhazledine_theme_posted_on(); ?>
-        </div><!-- .entry-meta -->
-        <?php
-        endif; ?>
-    </header><!-- .entry-header -->
+    <?php
+    the_date('M Y');
+    echo ' – ';
+    // Get reading time.
+    $reading_time = reading_time( get_the_content() );
+    $reading_time_string = parse_read_time( $reading_time );
+    echo '<span class="readingTime"><em>Read time: ' . $reading_time_string . '</em></span>';
+    ?>
 
-    <div class="entry-content">
-
-        <?php
-        // Get reading time.
-        $reading_time = reading_time( get_the_content() );
-        $reading_time_string = parse_read_time( $reading_time );
-        echo '<span class="readingTime"><em>Read time: ' . $reading_time_string . '</em></span>';
-        ?>
-        
-        <?php
-            // the_excerpt();
-        ?>
-    </div><!-- .entry-content -->
-
-    <footer class="entry-footer">
-        <?php //tomhazledine_theme_entry_footer(); ?>
-    </footer><!-- .entry-footer -->
 </article><!-- #post-## -->
