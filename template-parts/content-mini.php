0d52ab04cb8dcb1eb51903c48e31af4556d1954e
Tom Hazledine <tom@tomhazledine.com>
2016-11-15 15:17:46 +0000

    links and footnotes

--- template-parts/content-mini.php
+++ template-parts/content-mini.php
@@ -8,11 +8,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <h3 class="entry-title"><a href="<?= esc_url( get_permalink() ); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-
     <?php
     the_date('M Y');
     echo ' – ';
+    ?>
+    <h3><a href="<?= esc_url( get_permalink() ); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+
+    <?php
     // Get reading time.
     $reading_time = reading_time( get_the_content() );
     $reading_time_string = parse_read_time( $reading_time );
