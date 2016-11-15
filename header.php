8f49cf5af2c923df2a6e41c39cdb4d0dcde6b8b1
Tom Hazledine <tom@tomhazledine.com>
2016-11-15 16:34:21 +0000

    fixed headerfooter

--- header.php
+++ header.php
@@ -5,11 +5,13 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'tomhazledine_theme' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<p class="site-title"><?php bloginfo( 'name' ); ?></p>
+        <div class="header-inner">
+    		<p class="site-title"><?php bloginfo( 'name' ); ?></p>
 
-        <nav id="site-navigation" class="main-navigation" role="navigation">
-            <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-        </nav>
+            <nav id="site-navigation" class="main-navigation" role="navigation">
+                <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+            </nav>
+        </div>
 	</header>
 
 	<div id="content" class="site-content">
