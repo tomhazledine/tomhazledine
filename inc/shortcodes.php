731e4c0106da23856938dcc01135275eddd303a0
Tom Hazledine <tom@tomhazledine.com>
2016-11-18 09:07:16 +0000

    sidenote styles

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -2,6 +2,8 @@
 // [bartag foo="foo-value"]
 function sidenote_shortcode( $atts, $content = null ) {
 
+    print_pre($content);
+
     $output = '<span class="sidenote">';
         $output .= '<span class="visuallyhidden"> (</span>';
         $output .= WPCom_Markdown::get_instance()->transform( $content );
