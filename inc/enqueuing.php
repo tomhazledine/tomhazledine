bd3610228f08ad9f6306dde7f6aa0c3366a4ab02
Tom Hazledine <tom@tomhazledine.com>
2016-11-18 16:56:20 +0000

    moving scripts to footer

--- inc/enqueuing.php
+++ inc/enqueuing.php
@@ -13,11 +13,11 @@ function tomhazledine_theme_scripts() {
 
     // Use a non-WP version of jquery (to get around the native .noConflict() version that WP uses by default).
     wp_deregister_script('jquery');
-    wp_register_script('jquery', ("//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"), false, '1.11.2', true);
+    wp_register_script('jquery', ("//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"), false, '1.11.2', false, true);
     wp_enqueue_script('jquery');
 
 	// wp_enqueue_script( 'tomhazledine_theme-scripts', get_template_directory_uri() . '/assets/js/app.js', array( 'jquery' ), '20151215', true );
-    wp_enqueue_script( 'tomhazledine_theme-scripts', get_template_directory_uri() . '/assets/js/app.min.js', array( 'jquery' ), null );
+    wp_enqueue_script( 'tomhazledine_theme-scripts', get_template_directory_uri() . '/assets/js/app.min.js', array( 'jquery' ), false, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
