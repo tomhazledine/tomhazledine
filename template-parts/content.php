414bf98bbe4d0e8ff6b4fa3d307675ff0c4950aa
Tom Hazledine <tom@tomhazledine.com>
2016-11-24 08:32:39 +0000

    code cleanup

--- template-parts/content.php
+++ template-parts/content.php
@@ -25,29 +25,18 @@
 			echo '<span class="readingTime">Read time: ' . $reading_time_string . '</span>';
 			?>
 		</div>
-	</header><!-- .entry-header -->
+	</header>
 
 	<div class="entry-content">
 		
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'tomhazledine_theme' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'tomhazledine_theme' ),
-				'after'  => '</div>',
-			) );
-		?>
+		<?php the_content(); ?>
 
 		<div class="tweet-link-wrapper">
 			Found this post interesting or uselful? <?php tweet_this_link( get_the_title(), get_the_permalink(), 'Click here to share it on Twitter!'); ?>
 		</div>
-	</div><!-- .entry-content -->
+	</div>
 
 	<footer class="entry-footer">
 		<?php //tomhazledine_theme_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+	</footer>
+</article>
