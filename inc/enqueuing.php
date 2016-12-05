451f92f83237bfe5397471feb3ab3778e4652e89
Tom Hazledine <tom@tomhazledine.com>
2016-12-05 08:18:03 +0000

    moar enqueue-versioning shenaniganz

--- inc/enqueuing.php
+++ inc/enqueuing.php
@@ -18,7 +18,7 @@ function tomhazledine_theme_scripts() {
 
 	// wp_enqueue_script( 'tomhazledine_theme-scripts', get_template_directory_uri() . '/assets/js/app.js', array( 'jquery' ), '20151215', true );
     // wp_enqueue_script( 'tomhazledine_theme-scripts', get_template_directory_uri() . '/assets/js/app.min.js', array( 'jquery' ), false, true );
-    wp_enqueue_script( 'tomhazledine_theme-scripts', get_template_directory_uri() . '/assets/js/app.min.js', array(), false, true );
+    wp_enqueue_script( 'tomhazledine_theme-scripts', get_template_directory_uri() . '/assets/js/app.min.js', array(), null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
