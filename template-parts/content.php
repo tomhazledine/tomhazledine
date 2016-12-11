2488373eb1a09fa5a9596220fcc169f1dfab336d
Tom Hazledine <tom@tomhazledine.com>
2016-12-11 08:30:42 +0000

    removing comments from post-page

--- template-parts/content.php
+++ template-parts/content.php
@@ -37,9 +37,9 @@
 		
 		<?php
         // If comments are open or we have at least one comment, load up the comment template
-        if ( comments_open() || '0' != get_comments_number() ) {
-            comments_template( '', true );
-        }
+        // if ( comments_open() || '0' != get_comments_number() ) {
+        //     comments_template( '', true );
+        // }
     	?>
     	
 	</div>
