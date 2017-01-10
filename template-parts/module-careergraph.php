4ee371cb917da2d60eb135ef95ca1ddf835189d8
Tom Hazledine <tom@tomhazledine.com>
2017-01-10 11:29:35 +0000

    careergraph style tweaks

--- template-parts/module-careergraph.php
+++ template-parts/module-careergraph.php
@@ -1,42 +1,45 @@
 <div class="homeGraphWrapper">
-    <!-- <h2 class="homeGraphTitle"><em>fig #1: </em>My career aspirations over time&hellip;</h2> -->
+    
+    <div id="career-graph" class="homeGraph"></div>
+    
     <div class="homeGraphSelectors">
         <div class="homeGraphSelector checkWrapper">
             <label for="developer" class="visibleLabel selectorTitle">Developer</label>
-            <div class="check">
+            <div class="check dev">
                 <input type="checkbox" class="homegraphcheckbox" value="dev" id="developer" name="developer" checked/>
                 <label for="developer" class="symLabel"></label>
             </div>
         </div>
         <div class="homeGraphSelector checkWrapper">
             <label for="designer" class="visibleLabel selectorTitle">Designer</label>
-            <div class="check">
+            <div class="check des">
                 <input type="checkbox" class="homegraphcheckbox" value="des" id="designer" name="designer"/>
                 <label for="designer" class="symLabel"></label>
             </div>
         </div>
         <div class="homeGraphSelector checkWrapper">
             <label for="musician" class="visibleLabel selectorTitle">Musician</label>
-            <div class="check">
+            <div class="check mus">
                 <input type="checkbox" class="homegraphcheckbox" value="mus" id="musician" name="musician"/>
                 <label for="musician" class="symLabel"></label>
             </div>
         </div>
         <div class="homeGraphSelector checkWrapper">
             <label for="artist" class="visibleLabel selectorTitle">Artist</label>
-            <div class="check">
+            <div class="check art">
                 <input type="checkbox" class="homegraphcheckbox" value="art" id="artist" name="artist"/>
                 <label for="artist" class="symLabel"></label>
             </div>
         </div>
         <div class="homeGraphSelector checkWrapper">
             <label for="lego" class="visibleLabel selectorTitle">Lego Master Builder</label>
-            <div class="check">
+            <div class="check lego">
                 <input type="checkbox" class="homegraphcheckbox" value="lego" id="lego" name="lego"/>
                 <label for="lego" class="symLabel"></label>
             </div>
         </div>
     </div>
-    <div id="career-graph" class="homeGraph"></div>
+
     <span class="caption"><em>fig #1:</em> <strong>my career aspirations over time</strong></span>
+
 </div>
