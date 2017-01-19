deeaf119777f4e7334c94d3a993df2e7ed9bf0bf
Tom Hazledine <tom@tomhazledine.com>
2017-01-19 20:49:18 +0000

    breakpoint adjustment to optimize for ipads

--- template-parts/content-mini.php
+++ template-parts/content-mini.php
@@ -7,11 +7,15 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('post-snippet'); ?>>
     
     <code><?php the_date('M Y'); ?></code>
+
+    <!-- <br> -->
     
     <a href="<?= esc_url( get_permalink() ); ?>" rel="bookmark"><?php the_title(); ?></a>
+
+    <!-- <br> -->
 
     <?php
     // Get reading time.
