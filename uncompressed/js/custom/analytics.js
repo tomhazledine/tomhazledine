986d08096142b9d96ddcd11608e10e29d157c3d8
Tom Hazledine <tom@tomhazledine.com>
2017-02-26 10:31:39 +0000

    ga tracking code for intro note link

--- uncompressed/js/custom/analytics.js
+++ uncompressed/js/custom/analytics.js
@@ -7,5 +7,6 @@ for (var i = intro_note_link.length - 1; i >= 0; i--) {
 function track_intro_note_click(e) {
     e.preventDefault();
     var target_url = e.target.href;
+    ga('send', 'event', 'Intro Note Link', 'click', target_url);
     window.location = target_url;
 }
