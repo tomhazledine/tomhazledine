4981e46609dad1fec421c096a79919cde4a0a7ad
Tom Hazledine <tom@tomhazledine.com>
2017-03-01 17:45:37 +0000

    pitch values array

--- inc/shortcode-audio.php
+++ inc/shortcode-audio.php
@@ -32,6 +32,35 @@ function audio_module_shortcode( $atts ) {
     //  - key pitches
     //  - frequency scope?
     //  - volume
+    
+    $pitches = [
+        // Octave 4
+        ['pitch' => 261.63, 'note' => "C",  'type' => 'natural'],
+        ['pitch' => 277.18, 'note' => "C#", 'type' => 'accidental'],
+        ['pitch' => 293.66, 'note' => "D",  'type' => 'natural'],
+        ['pitch' => 311.13, 'note' => "D#", 'type' => 'accidental'],
+        ['pitch' => 329.63, 'note' => "E",  'type' => 'natural'],
+        ['pitch' => 349.23, 'note' => "F",  'type' => 'natural'],
+        ['pitch' => 369.99, 'note' => "F#", 'type' => 'accidental'],
+        ['pitch' => 392.00, 'note' => "G",  'type' => 'natural'],
+        ['pitch' => 415.30, 'note' => "G#", 'type' => 'accidental'],
+        ['pitch' => 440.00, 'note' => "A",  'type' => 'natural'],
+        ['pitch' => 466.16, 'note' => "A#", 'type' => 'accidental'],
+        ['pitch' => 493.88, 'note' => "B",  'type' => 'natural'],
+        // Octave 5
+        ['pitch' => 523.25, 'note' => "C",  'type' => 'natural'],
+        ['pitch' => 554.37, 'note' => "C#", 'type' => 'accidental'],
+        ['pitch' => 587.33, 'note' => "D",  'type' => 'natural'],
+        ['pitch' => 622.25, 'note' => "D#", 'type' => 'accidental'],
+        ['pitch' => 659.26, 'note' => "E",  'type' => 'natural'],
+        ['pitch' => 698.46, 'note' => "F",  'type' => 'natural'],
+        ['pitch' => 739.99, 'note' => "F#", 'type' => 'accidental'],
+        ['pitch' => 783.99, 'note' => "G",  'type' => 'natural'],
+        ['pitch' => 830.61, 'note' => "G#", 'type' => 'accidental'],
+        ['pitch' => 880.00, 'note' => "A",  'type' => 'natural'],
+        ['pitch' => 932.33, 'note' => "A#", 'type' => 'accidental'],
+        ['pitch' => 987.77, 'note' => "B",  'type' => 'natural'],
+    ];
 
     return $output;
 }
