d224215e4c69806cca1c1546ea8a7d7b1f418dee
Tom Hazledine <tom@tomhazledine.com>
2017-03-05 19:21:17 +0000

    testing basic note API functionality

--- uncompressed/js/static/audio-module-shortcode.js
+++ uncompressed/js/static/audio-module-shortcode.js
@@ -2,3 +2,22 @@ console.log('audio shortcode scripts loaded!');
 
 var wave = Sounds_API.handleWaveType('1');
 console.log(wave);
+
+var keys = document.getElementsByClassName('audio-module-key');
+
+// SETUP EVENT LISTENERS
+for (var i = 0; i < keys.length; i++) {
+    keys[i].addEventListener('mousedown',noteStart,false);
+    // keys[i].addEventListener('mouseover',_noteMouseover,false);
+    // keys[i].addEventListener('mouseout',_noteMouseout,false);
+    keys[i].addEventListener('mouseup',noteEnd,false);
+};
+
+function noteStart(){
+    var noteValue = this.getAttribute('data-pitch');
+    Sounds_API.noteStart( noteValue );
+}
+function noteEnd(){
+    var noteValue = this.getAttribute('data-pitch');
+    Sounds_API.noteEnd(noteValue);
+}
