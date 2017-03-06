8e336616471042c2bced4eaa4b5426ebb1b02aa8
Tom Hazledine <tom@tomhazledine.com>
2017-03-06 09:24:39 +0000

    analysis code cleanup

--- uncompressed/js/vendor/analysis.js
+++ uncompressed/js/vendor/analysis.js
@@ -29,144 +29,144 @@ var TOP_THRESHOLD = 20000;
  */
 
 // context = current audio API context
-// master = master audio output
-function audioAnalysis(context,master){
+// input = audio from source
+function audioAnalysis( context, input ){
     
     // Setup an analyser node
-    var volumeAnalyser = context.createAnalyser();
+    var volume_analyser = context.createAnalyser();
     // Set our FFT (Fast Fourier Transform) size
-    var fftSize = FFT_SIZE;
-    volumeAnalyser.fftSize = fftSize;
+    var fft_size = FFT_SIZE;
+    volume_analyser.fftSize = fft_size;
     // Get our sample rate (usually 44.1kHz, but we want to be certain as some systems are different)
-    var sampleRate = context.sampleRate;// returns in Hz (not kHz).
+    var sample_rate = context.sampleRate;// returns in Hz (not kHz).
     // Do we want our analyser to smooth the transitions for us?
     // If we do: set a time value. Otherwise set "0" to have no smoothing.
-    volumeAnalyser.smoothingTimeConstant = 0;
+    volume_analyser.smoothingTimeConstant = 0;
     // The buffer size is the number of "bins" of data
     // we get (e.g. the number of items in our array).
     // This will be half the FFT size.
-    var bufferSize = volumeAnalyser.frequencyBinCount;
-    // Connect the volumeAnalyser to our master audio output
-    master.connect(volumeAnalyser);
+    var buffer_size = volume_analyser.frequencyBinCount;
+    // Connect the volume_analyser to our input audio output
+    input.connect( volume_analyser );
 
     // Create a Node to listen to the output every
     // 2048 'frames' (a.k.a. 21 times a
     // second at sample-rate of 44.1kHz)
-    listenerNode = context.createScriptProcessor(2048,1,1);
+    listenerNode = context.createScriptProcessor( 2048, 1, 1 );
 
     // Connect it to our audio:
-    listenerNode.connect(master);
+    listenerNode.connect( input );
 
     listenerNode.onaudioprocess = function(){
 
         // Create an array to store our data
-        var array = new Uint8Array(bufferSize);
+        var array = new Uint8Array( buffer_size );
         // Get the audio data and store it in our array
-        volumeAnalyser.getByteFrequencyData(array);
+        volume_analyser.getByteFrequencyData( array );
         // If we wanted information about the waveform
         // rather than the frequency, we would use this:
-        // volumeAnalyser.getByteTimeDomainData(array);
+        // volume_analyser.getByteTimeDomainData(array);
         
         // Trim data to audible frequencies
-        array = trimFrequencies(array,sampleRate,fftSize);
+        array = _trim_frequencies( array, sample_rate, fft_size );
 
         // Calculate the mean value of the frequency frame
-        var volume = getAverageVolume(array);
+        var volume = _get_average_volume( array );
 
         // logArray(array);
 
-        var parsedArray = array;//parseFreqArray(array);
+        var parsed_array = array;//parseFreqArray(array);
 
         // Update the volume display
-        // console.log(volume);
+        // console.log( volume );
         // redrawVolume(volume);
-        // redrawFrequency(parsedArray);
+        // redrawFrequency(parsed_array);
 
     }
-}
 
-/**
- * -----------------------
- * TRIM OUR FREQUENCY DATA
- *
- * We only want to display
- * data for frequencies we
- * can hear (20Hz – 20kHz)
- * so trim any data above
- * and below the threshold
- * -----------------------
- */
-function trimFrequencies(bins,sampleRate,fftSize){
-    var bottomThreshold = BOTTOM_THRESHOLD;// 20Hz
-    var topThreshold = TOP_THRESHOLD;// 20kHz
+    /**
+     * -----------------------
+     * TRIM OUR FREQUENCY DATA
+     *
+     * We only want to display
+     * data for frequencies we
+     * can hear (20Hz – 20kHz)
+     * so trim any data above
+     * and below the threshold
+     * -----------------------
+     */
+    function _trim_frequencies( bins, sample_rate, fft_size ){
+        var bottom_threshold = BOTTOM_THRESHOLD;// 20Hz
+        var top_threshold = TOP_THRESHOLD;// 20kHz
 
-    var result = [];
+        var result = [];
 
-    for (var i = 1; i <= bins.length; i++) {
-        // console.log(i);
-        // Calculate the frequency for each "bin".
-        var frequency = i * sampleRate/fftSize;
-        if (frequency > bottomThreshold && frequency < topThreshold) {
-            var output = [];
-            output['frequency'] = frequency;
-            output['value'] = bins[i-1];// Strength of signal at selected frequency
-            result.push(output);
+        for ( var i = 1; i <= bins.length; i++ ) {
+            // console.log(i);
+            // Calculate the frequency for each "bin".
+            var frequency = i * sample_rate / fft_size;
+            if ( frequency > bottom_threshold && frequency < top_threshold ) {
+                var output = [];
+                output['frequency'] = frequency;
+                output['value'] = bins[ i - 1 ];// Strength of signal at selected frequency
+                result.push( output );
+            }
         }
+
+        // var start = [];
+        // start['frequency'] = 0;
+        // start['value'] = 0;
+        // result.unshift(start);
+
+        return result;
     }
 
-    // var start = [];
-    // start['frequency'] = 0;
-    // start['value'] = 0;
-    // result.unshift(start);
+    /**
+     * --------------------
+     * AVERAGE VOLUME
+     *
+     * Convert all the
+     * frequency amplitudes
+     * (for a given frame)
+     * into a single number
+     * --------------------
+     */
+    function _get_average_volume( array ){
+        var values = 0;
+        var count = 0;
+        var average = 0;
+        var length = array.length;
 
-    return result;
-}
-
-/**
- * --------------------
- * AVERAGE VOLUME
- *
- * Convert all the
- * frequency amplitudes
- * (for a given frame)
- * into a single number
- * --------------------
- */
-function getAverageVolume(array){
-    var values = 0;
-    var count = 0;
-    var average = 0;
-    var length = array.length;
-
-    // Sum all the frequency values
-    for (var i = 0; i < length; i++) {
-        if (array[i]['value'] > 0) {
-            values += array[i]['value'];
-            count++;
+        // Sum all the frequency values
+        for ( var i = 0; i < length; i++ ) {
+            if ( array[i]['value'] > 0 ) {
+                values += array[i]['value'];
+                count++;
+            }
         }
-    }
 
-    // Get the mean value
-    if (values > 0 && count > 0) {
-        average = values / count;
-    } else {
-        average = 0;
-    }
+        // Get the mean value
+        if ( values > 0 && count > 0 ) {
+            average = values / count;
+        } else {
+            average = 0;
+        }
 
-    // Limit to 2 decimal places
-    average = average.toFixed(2);
-    
-    // Remove any negative numbers
-    if (average < 0) {
-        average = 0;
-    }
+        // Limit to 2 decimal places
+        average = average.toFixed( 2 );
+        
+        // Remove any negative numbers
+        if ( average < 0 ) {
+            average = 0;
+        }
 
-    // Flatten peaks
-    if (average > 150) {
-        average = 150;
-    }
+        // Flatten peaks
+        if ( average > 150 ) {
+            average = 150;
+        }
 
-    return average;
+        return average;
+    }
 }
 
 /**
