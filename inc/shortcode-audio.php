66d324a1bebd6621ada089892847fe04b0581c79
Tom Hazledine <tom@tomhazledine.com>
2017-03-15 15:17:38 +0000

    volume layout

--- inc/shortcode-audio.php
+++ inc/shortcode-audio.php
@@ -25,92 +25,92 @@ function audio_module_shortcode( $atts ) {
     
     // Do we want to show a frequency graph, or an oscilloscope display?
     $type = !empty($atts['type']) ? $atts['type'] : 'frequency';
-
-    $output .= 'testing audio module';
-    $output .= ' | mode = ' . $type;
-
-    // Draw
-    //  - visualizer wrapper
-    //  - keyboard [with data-attr for pitch, set by $atts if available]
-    //  - volume
-    //  - player controls (if wanted): play/pause | loop
-    //  - wave type selectors
-    // Load JS
-    //  - wave type
-    //  - key pitches
-    //  - frequency scope?
-    //  - volume
     
-    $pitches = [
-        // Octave 4
-        ['pitch' => 261.63, 'note' => "C",  'type' => 'natural'],
-        ['pitch' => 277.18, 'note' => "C#", 'type' => 'accidental'],
-        ['pitch' => 293.66, 'note' => "D",  'type' => 'natural'],
-        ['pitch' => 311.13, 'note' => "D#", 'type' => 'accidental'],
-        ['pitch' => 329.63, 'note' => "E",  'type' => 'natural'],
-        ['pitch' => 349.23, 'note' => "F",  'type' => 'natural'],
-        ['pitch' => 369.99, 'note' => "F#", 'type' => 'accidental'],
-        ['pitch' => 392.00, 'note' => "G",  'type' => 'natural'],
-        ['pitch' => 415.30, 'note' => "G#", 'type' => 'accidental'],
-        ['pitch' => 440.00, 'note' => "A",  'type' => 'natural'],
-        ['pitch' => 466.16, 'note' => "A#", 'type' => 'accidental'],
-        ['pitch' => 493.88, 'note' => "B",  'type' => 'natural'],
-        // Octave 5
-        ['pitch' => 523.25, 'note' => "C",  'type' => 'natural'],
-        ['pitch' => 554.37, 'note' => "C#", 'type' => 'accidental'],
-        ['pitch' => 587.33, 'note' => "D",  'type' => 'natural'],
-        ['pitch' => 622.25, 'note' => "D#", 'type' => 'accidental'],
-        ['pitch' => 659.26, 'note' => "E",  'type' => 'natural'],
-        ['pitch' => 698.46, 'note' => "F",  'type' => 'natural'],
-        ['pitch' => 739.99, 'note' => "F#", 'type' => 'accidental'],
-        ['pitch' => 783.99, 'note' => "G",  'type' => 'natural'],
-        ['pitch' => 830.61, 'note' => "G#", 'type' => 'accidental'],
-        ['pitch' => 880.00, 'note' => "A",  'type' => 'natural'],
-        ['pitch' => 932.33, 'note' => "A#", 'type' => 'accidental'],
-        ['pitch' => 987.77, 'note' => "B",  'type' => 'natural'],
-    ];
+    $output .= '<div class="audio-module-container clearfix">';
 
-    $keyboard = '<ul class="audio-module-keyboard clearfix">';
+        // Draw
+        //  - visualizer wrapper
+        //  - keyboard [with data-attr for pitch, set by $atts if available]
+        //  - volume
+        //  - player controls (if wanted): play/pause | loop
+        //  - wave type selectors
+        // Load JS
+        //  - wave type
+        //  - key pitches
+        //  - frequency scope?
+        //  - volume
+        
+        $pitches = [
+            // Octave 4
+            ['pitch' => 261.63, 'note' => "C",  'type' => 'natural'],
+            ['pitch' => 277.18, 'note' => "C#", 'type' => 'accidental'],
+            ['pitch' => 293.66, 'note' => "D",  'type' => 'natural'],
+            ['pitch' => 311.13, 'note' => "D#", 'type' => 'accidental'],
+            ['pitch' => 329.63, 'note' => "E",  'type' => 'natural'],
+            ['pitch' => 349.23, 'note' => "F",  'type' => 'natural'],
+            ['pitch' => 369.99, 'note' => "F#", 'type' => 'accidental'],
+            ['pitch' => 392.00, 'note' => "G",  'type' => 'natural'],
+            ['pitch' => 415.30, 'note' => "G#", 'type' => 'accidental'],
+            ['pitch' => 440.00, 'note' => "A",  'type' => 'natural'],
+            ['pitch' => 466.16, 'note' => "A#", 'type' => 'accidental'],
+            ['pitch' => 493.88, 'note' => "B",  'type' => 'natural'],
+            // Octave 5
+            ['pitch' => 523.25, 'note' => "C",  'type' => 'natural'],
+            ['pitch' => 554.37, 'note' => "C#", 'type' => 'accidental'],
+            ['pitch' => 587.33, 'note' => "D",  'type' => 'natural'],
+            ['pitch' => 622.25, 'note' => "D#", 'type' => 'accidental'],
+            ['pitch' => 659.26, 'note' => "E",  'type' => 'natural'],
+            ['pitch' => 698.46, 'note' => "F",  'type' => 'natural'],
+            ['pitch' => 739.99, 'note' => "F#", 'type' => 'accidental'],
+            ['pitch' => 783.99, 'note' => "G",  'type' => 'natural'],
+            ['pitch' => 830.61, 'note' => "G#", 'type' => 'accidental'],
+            ['pitch' => 880.00, 'note' => "A",  'type' => 'natural'],
+            ['pitch' => 932.33, 'note' => "A#", 'type' => 'accidental'],
+            ['pitch' => 987.77, 'note' => "B",  'type' => 'natural'],
+        ];
 
-    foreach ($pitches as $pitch) {
-        $keyboard .= '<li class="audio-module-key ' . $pitch['type'] . '" data-pitch="' . $pitch['pitch'] . '">';
-        $keyboard .= '<span class="visuallyhidden">' . $pitch['note'] . '</span>';
-        $keyboard .= '</li>';
-    }
+        $keyboard = '<ul class="audio-module-keyboard clearfix">';
 
-    $keyboard .= '</ul>';
+        foreach ($pitches as $pitch) {
+            $keyboard .= '<li class="audio-module-key ' . $pitch['type'] . '" data-pitch="' . $pitch['pitch'] . '">';
+            $keyboard .= '<span class="visuallyhidden">' . $pitch['note'] . '</span>';
+            $keyboard .= '</li>';
+        }
 
-    $output .= $keyboard;
+        $keyboard .= '</ul>';
 
-    $volume_1_display = '
-        <div id="volume-1-section" class="volume-section">
-            <div id="volume-1-display-wrapper" class="volume-display-wrapper">
-                <div id="volume-1-display" class="volume-display"></div>
-                <div id="volume-1-peak" class="volume-peak"></div>
-            </div>
-            <span class="volume-label max">0dB</span>
-            <span class="volume-label min">-96dB</span>
-        </div>';
-    $volume_2_display = '
-        <div id="volume-2-section" class="volume-section">
-            <div id="volume-2-display-wrapper" class="volume-display-wrapper">
-                <div id="volume-2-display" class="volume-display"></div>
-                <div id="volume-2-peak" class="volume-peak"></div>
-            </div>
-            <span class="volume-label max">0dB</span>
-            <span class="volume-label min">-96dB</span>
-        </div>';
+        $output .= $keyboard;
 
-    $output .= '<div class="clearfix">';
-        $output .= $volume_1_display;
-
-        $output .= '<div class="controlStrip clearfix">
-                <div class="controlStripInner">
-                    <input class="pianoController master_gain" id="master_gain" data-controlName="master_gain" type="range" min="0" max="1" value="0.8" step="0.01">
+        $volume_1_display = '
+            <div id="volume-1-section" class="volume-section">
+                <div id="volume-1-display-wrapper" class="volume-display-wrapper">
+                    <div id="volume-1-display" class="volume-display"></div>
+                    <div id="volume-1-peak" class="volume-peak"></div>
                 </div>
+                <span class="volume-label max">0dB</span>
+                <span class="volume-label min">-96dB</span>
+            </div>';
+        $volume_2_display = '
+            <div id="volume-2-section" class="volume-section">
+                <div id="volume-2-display-wrapper" class="volume-display-wrapper">
+                    <div id="volume-2-display" class="volume-display"></div>
+                    <div id="volume-2-peak" class="volume-peak"></div>
+                </div>
+                <span class="volume-label max">0dB</span>
+                <span class="volume-label min">-96dB</span>
             </div>';
 
-        $output .= $volume_2_display;
+        $output .= '<div class="volume-sections clearfix">';
+            $output .= $volume_1_display;
+
+            $output .= '<div class="controlStrip clearfix">
+                    <div class="controlStripInner">
+                        <input class="pianoController master_gain" id="master_gain" data-controlName="master_gain" type="range" min="0" max="1" value="0.8" step="0.01">
+                    </div>
+                </div>';
+
+            $output .= $volume_2_display;
+        $output .= '</div>';
     $output .= '</div>';
 
     // Re-open main content.
