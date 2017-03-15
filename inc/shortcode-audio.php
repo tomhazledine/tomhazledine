44e24c456e936c1750a261d8790ff31587c05383
Tom Hazledine <tom@tomhazledine.com>
2017-03-15 16:32:59 +0000

    responsive styles

--- inc/shortcode-audio.php
+++ inc/shortcode-audio.php
@@ -42,37 +42,37 @@ function audio_module_shortcode( $atts ) {
         
         $pitches = [
             // Octave 4
-            ['pitch' => 261.63, 'note' => "C",  'type' => 'natural'],
-            ['pitch' => 277.18, 'note' => "C#", 'type' => 'accidental'],
-            ['pitch' => 293.66, 'note' => "D",  'type' => 'natural'],
-            ['pitch' => 311.13, 'note' => "D#", 'type' => 'accidental'],
-            ['pitch' => 329.63, 'note' => "E",  'type' => 'natural'],
-            ['pitch' => 349.23, 'note' => "F",  'type' => 'natural'],
-            ['pitch' => 369.99, 'note' => "F#", 'type' => 'accidental'],
-            ['pitch' => 392.00, 'note' => "G",  'type' => 'natural'],
-            ['pitch' => 415.30, 'note' => "G#", 'type' => 'accidental'],
-            ['pitch' => 440.00, 'note' => "A",  'type' => 'natural'],
-            ['pitch' => 466.16, 'note' => "A#", 'type' => 'accidental'],
-            ['pitch' => 493.88, 'note' => "B",  'type' => 'natural'],
+            ['octave' => 4, 'pitch' => 261.63, 'note' => "C",  'type' => 'natural'],
+            ['octave' => 4, 'pitch' => 277.18, 'note' => "C#", 'type' => 'accidental'],
+            ['octave' => 4, 'pitch' => 293.66, 'note' => "D",  'type' => 'natural'],
+            ['octave' => 4, 'pitch' => 311.13, 'note' => "D#", 'type' => 'accidental'],
+            ['octave' => 4, 'pitch' => 329.63, 'note' => "E",  'type' => 'natural'],
+            ['octave' => 4, 'pitch' => 349.23, 'note' => "F",  'type' => 'natural'],
+            ['octave' => 4, 'pitch' => 369.99, 'note' => "F#", 'type' => 'accidental'],
+            ['octave' => 4, 'pitch' => 392.00, 'note' => "G",  'type' => 'natural'],
+            ['octave' => 4, 'pitch' => 415.30, 'note' => "G#", 'type' => 'accidental'],
+            ['octave' => 4, 'pitch' => 440.00, 'note' => "A",  'type' => 'natural'],
+            ['octave' => 4, 'pitch' => 466.16, 'note' => "A#", 'type' => 'accidental'],
+            ['octave' => 4, 'pitch' => 493.88, 'note' => "B",  'type' => 'natural'],
             // Octave 5
-            ['pitch' => 523.25, 'note' => "C",  'type' => 'natural'],
-            ['pitch' => 554.37, 'note' => "C#", 'type' => 'accidental'],
-            ['pitch' => 587.33, 'note' => "D",  'type' => 'natural'],
-            ['pitch' => 622.25, 'note' => "D#", 'type' => 'accidental'],
-            ['pitch' => 659.26, 'note' => "E",  'type' => 'natural'],
-            ['pitch' => 698.46, 'note' => "F",  'type' => 'natural'],
-            ['pitch' => 739.99, 'note' => "F#", 'type' => 'accidental'],
-            ['pitch' => 783.99, 'note' => "G",  'type' => 'natural'],
-            ['pitch' => 830.61, 'note' => "G#", 'type' => 'accidental'],
-            ['pitch' => 880.00, 'note' => "A",  'type' => 'natural'],
-            ['pitch' => 932.33, 'note' => "A#", 'type' => 'accidental'],
-            ['pitch' => 987.77, 'note' => "B",  'type' => 'natural'],
+            ['octave' => 5, 'pitch' => 523.25, 'note' => "C",  'type' => 'natural'],
+            ['octave' => 5, 'pitch' => 554.37, 'note' => "C#", 'type' => 'accidental'],
+            ['octave' => 5, 'pitch' => 587.33, 'note' => "D",  'type' => 'natural'],
+            ['octave' => 5, 'pitch' => 622.25, 'note' => "D#", 'type' => 'accidental'],
+            ['octave' => 5, 'pitch' => 659.26, 'note' => "E",  'type' => 'natural'],
+            ['octave' => 5, 'pitch' => 698.46, 'note' => "F",  'type' => 'natural'],
+            ['octave' => 5, 'pitch' => 739.99, 'note' => "F#", 'type' => 'accidental'],
+            ['octave' => 5, 'pitch' => 783.99, 'note' => "G",  'type' => 'natural'],
+            ['octave' => 5, 'pitch' => 830.61, 'note' => "G#", 'type' => 'accidental'],
+            ['octave' => 5, 'pitch' => 880.00, 'note' => "A",  'type' => 'natural'],
+            ['octave' => 5, 'pitch' => 932.33, 'note' => "A#", 'type' => 'accidental'],
+            ['octave' => 5, 'pitch' => 987.77, 'note' => "B",  'type' => 'natural'],
         ];
 
         $keyboard = '<ul class="audio-module-keyboard clearfix">';
 
         foreach ($pitches as $pitch) {
-            $keyboard .= '<li class="audio-module-key ' . $pitch['type'] . '" data-pitch="' . $pitch['pitch'] . '">';
+            $keyboard .= '<li class="audio-module-key ' . $pitch['type'] . ' octave-' . $pitch['octave'] . '" data-pitch="' . $pitch['pitch'] . '">';
             $keyboard .= '<span class="visuallyhidden">' . $pitch['note'] . '</span>';
             $keyboard .= '</li>';
         }
@@ -104,6 +104,7 @@ function audio_module_shortcode( $atts ) {
             $output .= $volume_1_display;
 
             $output .= '<div class="controlStrip clearfix">
+                    <span class="controlStrip-label">Gain</span>
                     <div class="controlStripInner">
                         <input class="pianoController master_gain" id="master_gain" data-controlName="master_gain" type="range" min="0" max="1" value="0.8" step="0.01">
                     </div>
