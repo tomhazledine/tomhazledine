f7ac53a911f6fc4541d40bc6c11fa1563f24ec8f
Tom Hazledine <tom@tomhazledine.com>
2017-03-25 08:14:15 +0000

    removing log

--- uncompressed/js/custom/tweet.js
+++ uncompressed/js/custom/tweet.js
@@ -1,8 +1,6 @@
 var tweet_form = document.getElementById('tweet-form');
 var tweet_form_submit = document.getElementById('tweet-form-submit');
 
-
-console.log(tweet_form);
 tweet_form.addEventListener('submit',tweet_form_submission,false);
 
 function tweet_form_submission(e){
