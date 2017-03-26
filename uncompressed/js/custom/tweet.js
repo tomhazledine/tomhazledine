bc28dd8814de79f33f1baec497496f3fbc44e200
Tom Hazledine <tom@tomhazledine.com>
2017-03-26 09:18:17 +0100

    check tweet-form exists before running js

--- uncompressed/js/custom/tweet.js
+++ uncompressed/js/custom/tweet.js
@@ -1,7 +1,11 @@
 var tweet_form = document.getElementById('tweet-form');
-var tweet_form_submit = document.getElementById('tweet-form-submit');
 
-tweet_form.addEventListener('submit',tweet_form_submission,false);
+
+if (tweet_form) {
+    var tweet_form_submit = document.getElementById('tweet-form-submit');
+
+    tweet_form.addEventListener('submit',tweet_form_submission,false);
+}
 
 function tweet_form_submission(e){
     e.preventDefault();
