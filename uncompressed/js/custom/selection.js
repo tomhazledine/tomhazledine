04164aa4c45ea3a83330e920d6431df6b7508e1a
Tom Hazledine <tom.hazledine@creation.agency>
2018-07-22 15:29:53 +0100

    adding old js functions to webpack build

--- uncompressed/js/custom/selection.js
+++ uncompressed/js/custom/selection.js
@@ -14,7 +14,7 @@
 
 // We sometimes want our selection-notifier position to
 // persist, so we'll make a global variable for it.
-var global_position = [x=0,y=0];
+var global_position = { x: 0, y: 0 };
 
 // We'll always want to use the same element as a wrapper
 // for our widget. This is it.
@@ -30,53 +30,46 @@ var tweet_widget_inner = document.getElementById('tweet-widget-inner');
  * selected.
  * ---------------------
  */
-function selection_handler(event){
-
+function selection_handler(event) {
     // console.log(event.target);
     var click_target = event.target;
-    var selectable_wrapper = document.getElementsByClassName( 'selectable-area' );
+    var selectable_wrapper = document.getElementsByClassName('selectable-area');
 
     for (var i = selectable_wrapper.length - 1; i >= 0; i--) {
         var parent = selectable_wrapper[i];
-        var is_child = check_relationship( parent, click_target );
+        var is_child = check_relationship(parent, click_target);
     }
 
     // Only run the selection-code if the click
     // happens inside a `.selectable-area` element.
-    if ( is_child ) {
-
+    if (is_child) {
         // Get the selected text
         var selection = get_selection();
 
         // If there is a selection & the selection
         // is more than 15 characters long...
-        if ( selection && selection.length > 15) {
-
+        if (selection && selection.length > 15) {
             // Turn selection into "tweet" object.
             var tweet_object = build_tweet_content(selection);
-            
+
             // Set the link for the widget.
             tweet_widget.setAttribute('href', tweet_object.url);
-            
+
             // Make the widget visible.
             tweet_widget.style.display = 'block';
 
             // Set the position for the tweet widget (using the
             // global vars set by the get_selection function).
-            tweet_widget.style.top = global_position['x'] + 'px';
-            tweet_widget.style.left = global_position['y'] + 'px';
-            
+            tweet_widget.style.top = global_position.x + 'px';
+            tweet_widget.style.left = global_position.y + 'px';
         } else {
-
             // Hide the widget if there's no content to display.
             tweet_widget.style.display = 'none';
         }
     } else {
-
         // Hide the widget if there's no content to display.
         tweet_widget.style.display = 'none';
     }
-
 }
 
 /**
@@ -90,8 +83,8 @@ function selection_handler(event){
  * pressed.
  * ---------------------------
  */
-document.addEventListener('keyup',selection_handler,false);
-document.addEventListener('mouseup',selection_handler,false);
+document.addEventListener('keyup', selection_handler, false);
+document.addEventListener('mouseup', selection_handler, false);
 
 /**
  * ---------------------
@@ -102,7 +95,7 @@ document.addEventListener('mouseup',selection_handler,false);
  * element-2.
  * ---------------------
  */
-function check_relationship( parent, child ) {
+function check_relationship(parent, child) {
     var node = child.parentNode;
     while (node != null) {
         if (node == parent) {
@@ -123,11 +116,11 @@ function check_relationship( parent, child ) {
  * return false.
  * ----------------------
  */
-function get_selection_position(event){
-    var position = [];
+function get_selection_position(event) {
+    var position = {};
     if (event.clientX) {
-        position['x'] = event.clientX;
-        position['y'] = event.clientY;
+        position.x = event.clientX;
+        position.y = event.clientY;
         global_position = position;
     } else {
         position = false;
@@ -139,46 +132,44 @@ function get_selection_position(event){
  * ---------------------
  * GET SELECTED TEXT
  *
- * If some text has been 
+ * If some text has been
  * selected, get it. If
  * not, return false.
  * ---------------------
  */
-function get_selection(){
+function get_selection() {
     var text = false;
     if (window.getSelection) {
         var selection = window.getSelection();
         text = selection.toString();
         if (text.length > 0 && text != ' ') {
-
             var range = selection.getRangeAt(0);
             var range_bounding_rect = range.getBoundingClientRect();
             var calculated_to_position = document.body.scrollTop + range_bounding_rect.top;
 
             var center_point = (range_bounding_rect.left + range_bounding_rect.right) / 2;
 
-            global_position['x'] = Math.round(calculated_to_position);
-            global_position['y'] = Math.round(center_point);
+            global_position.x = Math.round(calculated_to_position);
+            global_position.y = Math.round(center_point);
         }
-    } else if (document.selection && document.selection.type != "Control") {
+    } else if (document.selection && document.selection.type != 'Control') {
         text = document.selection.createRange().text;
     }
-    
+
     return text;
 }
 
 /**
  * --------------------------------
  * BUILD TWEET URL AND CONTENT
- * 
+ *
  * Trim the text to the appropriate
  * length, then parse the text into
  * tweet-friendly format, and add a
  * page-link and my username.
  * --------------------------------
  */
-function build_tweet_content(text){
-
+function build_tweet_content(text) {
     // NOTE: use %23 rather than # if adding a hashtag.
 
     // Setup an object to hold our results.
@@ -197,31 +188,37 @@ function build_tweet_content(text){
     var max_length = 140;
 
     // How many characters is our username?
-    var username_length = username.length + 1;// "1" accounts for a space.
+    var username_length = username.length + 1; // "1" accounts for a space.
 
     // How many characters is the page URL?
-    var link_length = link.length + 1;// "1" accounts for a space.
-    
+    var link_length = link.length + 1; // "1" accounts for a space.
+
     // Calculate how many characters we have left over for text.
     var max_tweet_length = max_length - username_length - tco_length;
 
     // Crop our text to fit the remaining character-count.
     if (text.length > max_tweet_length) {
-        var trimmed_text = text.substring( 0, (max_tweet_length - 3) );
+        var trimmed_text = text.substring(0, max_tweet_length - 3);
         // Remove start/end spaces.
-        trimmed_text = trimmed_text.replace(/^\s+|\s+$/g,'');
+        trimmed_text = trimmed_text.replace(/^\s+|\s+$/g, '');
         // Add an ellipsis if the text has been cropped.
         trimmed_text = trimmed_text + '…';
     } else {
         // Remove start/end spaces.
-        var trimmed_text = text.replace(/^\s+|\s+$/g,'');
+        var trimmed_text = text.replace(/^\s+|\s+$/g, '');
     }
 
     // Replace spaces with "+" (so the sharing-link works).
-    var parsed_text = trimmed_text.replace(/ /gi,'+');
+    var parsed_text = trimmed_text.replace(/ /gi, '+');
 
     // Build the full tweet link.
-    var tweet_href = 'https://twitter.com/intent/tweet?source=webclient&amp;text=' + parsed_text +  '+' + link + '+' + username;
+    var tweet_href =
+        'https://twitter.com/intent/tweet?source=webclient&amp;text=' +
+        parsed_text +
+        '+' +
+        link +
+        '+' +
+        username;
 
     // Save our results to our "results" object.
     result.url = tweet_href;
--- uncompressed/js/main.js
+++ uncompressed/js/main.js
@@ -1,4 +1,16 @@
 import './icons';
 import '../scss/main.scss';
 
+import './custom/_s_navigation';
+import './custom/_s_skip-link-focus-fix';
+import './custom/analytics';
 import './custom/careergraph';
+import './custom/selection';
+import './custom/sidenotes';
+import './custom/tweet';
+
+import Prism from 'prismjs';
+
+import hljs from 'highlight.js';
+// import './vendor/highlight.pack';
+hljs.initHighlightingOnLoad();
