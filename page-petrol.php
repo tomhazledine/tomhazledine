c49bb668fea591d68bbb1642972603f6aee066b2
Tom Hazledine <tom.hazledine@creation.agency>
2018-07-30 11:25:14 +0100

    petrol vis template

---
+++ page-petrol.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Petrol Data Vis
+ *
+ * @package Tom_Hazledine_Theme
+ */?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>React Sandbox</title>
+    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/petrol/petrol.css">
+</head>
+<body>
+    <div id="root"></div>
+    <script src="<?= get_template_directory_uri(); ?>/petrol/petrol.js"></script>
+</body>
+</html>
